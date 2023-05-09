f7b4035f9594cc33e6bda536166ab831e892c43a
illyum <90023277+itzilly@users.noreply.github.com>
2023-05-09 15:07:56 -0600

    Create TODO.py

---
+++ TODO.py
@@ -0,0 +1,6 @@
+# TODO: Migrate Repositories
+# TODO: Fix Leaderboards
+# TODO: Create Bot Start Scripts (bat/sh)
+# TODO: Move database to API and away from bot
+# TODO: Fix update-divisions command
+# TODO: Add dynamic cog loader/unloader/reloader
