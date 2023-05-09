6457ffdff47a74ab176bad2039b8d66b153de6a0
illyum <90023277+itzilly@users.noreply.github.com>
2023-05-09 15:26:10 -0600

    Update TODO's

--- TODO.py
+++ TODO.py
@@ -4,3 +4,6 @@
 # TODO: Move database to API and away from bot
 # TODO: Fix update-divisions command
 # TODO: Add dynamic cog loader/unloader/reloader
+# TODO: Create custom logging solution
+# TODO: Add argument to disable cacheing
+# TODO: Add argument to change cache entry lifetime
