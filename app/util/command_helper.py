b77a702f0f13a18299c1da942a6f056c03ab32cf
illyum <90023277+itzilly@users.noreply.github.com>
2023-06-03 05:14:10 -0600

    Rework admin perms checker

--- app/util/command_helper.py
+++ app/util/command_helper.py
@@ -1,29 +1,43 @@
+import discord
 import logging
-import util.local as fs
 
-from util.embed_lib import *
+from util.local import LOCAL_DATA
+from util.embed_lib import InsufficientPermissionsEmbed
 
 
-async def ensure_bot_permissions(interaction: discord.Interaction, send_deny_response=False):
-	has_permission = False
+async def ensure_bot_perms(interaction: discord.Interaction, send_denied_response: bool = False) -> bool:
+	"""
+	Checks weather a user has sufficient permissions to execute an admin command.
+	This checks the admin level within the config file for admin role id, or if the
+	user has 'Administrator' permission in the same guild as the bot.
 
+	Parameters:
+		interaction (discord.interaction): the interaction from a command or event
+		send_denied_response (bool): weather to send an embed response if user has insufficient perms
+
+	Returns:
+		A boolean for if user has sufficient permissions.
+	"""
 	user = interaction.user
-
 	logging.debug(f"Checking bot permissions for {user}")
-	cmd = "SELECT bot_admin_role_id FROM config"
-	request = fs.LOCAL_DATA.cursor.execute(cmd).fetchone()
 
-	try:
-		bot_role_id = int(request[0])
-		if user.get_role(bot_role_id):
-			logging.debug("Located role ID")
-			has_permission = True
-	except TypeError:
-		logging.error("No bot token found")
-		if send_deny_response:
+	if user.guild_permissions.administrator:
+		return True
+
+	admin_role_id = LOCAL_DATA.config.get("role_ids", "bot_admin")
+	if admin_role_id is None:
+		if send_denied_response:
 			await interaction.response.send_message(embed=InsufficientPermissionsEmbed())
 		return False
 
-	if send_deny_response and not has_permission:
-		await interaction.response.send_message(embed=InsufficientPermissionsEmbed())
-	return has_permission
+	user_role_ids = [role.id for role in user.roles]
+	if admin_role_id in user_role_ids:
+		return True
+
+	else:
+		if send_denied_response:
+			await interaction.response.send_message(embed=InsufficientPermissionsEmbed())
+		return False
+
+
+
