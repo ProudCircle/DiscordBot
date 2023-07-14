1869733f2004c9ae33df1fd81104c24970a2f9b7
illyum <90023277+itzilly@users.noreply.github.com>
2023-07-14 11:38:24 -0600

    Add admin commands for linking

--- app/extensions/link_command.py
+++ app/extensions/link_command.py
@@ -21,8 +21,9 @@ from util.mcign import MCIGN
 from discord import app_commands
 from discord.ext import commands
 from util import local, embed_lib
-from util.embed_lib import SuccessfullyUnlinkedEmbed, InsufficientPermissionsEmbed
-
+from util.command_helper import ensure_bot_perms
+from util.embed_lib import SuccessfullyUnlinkedEmbed, InsufficientPermissionsEmbed, InvalidArgumentEmbed, \
+    SuccessfullyForceUnlinkedEmbed
 
 how_to_link = discord.Embed(colour=discord.Colour(0xfa1195))
 how_to_link.add_field(
@@ -108,9 +109,94 @@ class UnlinkCommand(commands.Cog):
         await interaction.edit_original_response(embed=SuccessfullyUnlinkedEmbed(interaction.user.mention))
 
 
+class ForceLinkCommand(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.local_data: local.LocalDataSingleton = local.LOCAL_DATA
+
+    def get_player(self, player_id):
+        player = self.local_data.uuid_cache.get_entry(player_id)
+        if player is None:
+            player = MCIGN(player_id)
+            local.LOCAL_DATA.uuid_cache.add_entry(player.uuid, player.name)
+        return player
+
+    @app_commands.command(name="forcelink", description="Force Link a discord user and ign/uuid")
+    @app_commands.describe(discord_id="Discord ID associated with minecraft username")
+    @app_commands.describe(player="Uuid/Name of player to force link")
+    async def force_link_command(self, interaction: discord.Interaction, discord_id: str, player: str):
+        await interaction.response.defer()
+        is_allowed = await ensure_bot_perms(interaction, send_denied_response=True)
+        if not is_allowed:
+            return
+
+        try:
+            discord_id = int(discord_id)
+        except Exception as e:
+            await interaction.edit_original_response(embed=embed_lib.InvalidArgumentEmbed())
+
+        mojang_player = MCIGN(player)
+
+        # Make sure their account isn't already linked
+        server_id = int(local.LOCAL_DATA.config.get("bot", "server_id"))
+        force_linked_discord_user = self.bot.get_guild(server_id).get_member(discord_id)
+        if force_linked_discord_user is None:
+            await interaction.edit_original_response(embed=embed_lib.InvalidArgumentEmbed())
+        link = local.LOCAL_DATA.discord_link.get_link(discord_id)
+        if link is not None:  # AKA Their account IS linked
+            already_linked_embed = discord.Embed(colour=discord.Colour(0x820529))
+            already_linked_embed.description = "This account is already linked"
+            await interaction.edit_original_response(embed=already_linked_embed)
+            return
+
+        # Make API call to make sure username is linked to a valid Mojang account
+        if mojang_player.uuid is None or mojang_player.name is None:
+            await interaction.edit_original_response(embed=embed_lib.InvalidMojangUserEmbed(player=player))
+            return
+
+        # Bypass api security check
+        forced_discord_user_discrim = f"{force_linked_discord_user.name}#{force_linked_discord_user.discriminator}"
+        local.LOCAL_DATA.discord_link.register_link(mojang_player.uuid, discord_id, forced_discord_user_discrim)
+        successful_embed = embed_lib.SuccessfullyForceLinkedEmbed(mojang_player.name, force_linked_discord_user)
+        await interaction.edit_original_response(embed=successful_embed)
+
+
+class ForceUnlinkCommand(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.local_data: local.LocalDataSingleton = local.LOCAL_DATA
+
+    def get_player(self, player_id):
+        player = self.local_data.uuid_cache.get_entry(player_id)
+        if player is None:
+            player = MCIGN(player_id)
+            local.LOCAL_DATA.uuid_cache.add_entry(player.uuid, player.name)
+        return player
+
+    @app_commands.command(name="forceunlink", description="Force Unlink a discord user and ign/uuid")
+    @app_commands.describe(id="Discord ID or UUID of a player to remove")
+    async def force_unlink(self, interaction: discord.Interaction, id: str):
+        await interaction.response.defer()
+        is_allowed = await ensure_bot_perms(interaction, send_denied_response=True)
+        if not is_allowed:
+            return
+
+        link = self.local_data.discord_link.get_link(id)
+        if link is None:
+            await interaction.edit_original_response(embed=InvalidArgumentEmbed())
+            return
+
+        self.local_data.discord_link.remove_link(link.uuid)
+        await interaction.edit_original_response(embed=SuccessfullyForceUnlinkedEmbed(link.discord_username))
+
+
 # Add link and unlink commands to bot
 async def setup(bot: commands.Bot):
-    logging.debug("Adding cog: LinkCommand")
+    logging.debug("Adding Cog: LinkCommand")
     await bot.add_cog(LinkCommand(bot))
     logging.debug("Adding Cog: UnlinkCommand")
     await bot.add_cog(UnlinkCommand(bot))
+    logging.debug("Adding Cog: ForceLinkCommand")
+    await bot.add_cog(ForceLinkCommand(bot))
+    logging.debug("Adding Cog: ForceUnlinkCommand")
+    await bot.add_cog(ForceUnlinkCommand(bot))
