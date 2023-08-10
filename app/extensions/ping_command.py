3a438a5d80a326807aafd032f7cb4d4b8dcf8034
illyum <90023277+itzilly@users.noreply.github.com>
2023-08-10 08:59:28 -0600

    Update ping_command.py

--- app/extensions/ping_command.py
+++ app/extensions/ping_command.py
@@ -29,7 +29,8 @@ class PingCommand(commands.Cog):
     @app_commands.command(name="alive", description="Check if the bot is alive")
     async def ping(self, interaction: discord.Interaction):
         alive_embed = discord.Embed(description="I'm alive!", colour=discord.Colour(0xeb07a6))
-        await interaction.response.send_message(embed=alive_embed)
+        # await interaction.response.send_message(embed=alive_embed)
+        await interaction.response.send_message("@360541203751763970 hello!")
 
 
 async def setup(bot: commands.Bot):
