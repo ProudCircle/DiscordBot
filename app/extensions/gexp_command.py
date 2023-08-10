63558a1418b9db84d69be7f8ed664f9df28b6847
illyum <90023277+itzilly@users.noreply.github.com>
2023-08-10 08:32:39 -0600

    Update gexp_command.py

--- app/extensions/gexp_command.py
+++ app/extensions/gexp_command.py
@@ -18,40 +18,67 @@ class GexpCommand(commands.GroupCog, name="gexp"):
 
     @app_commands.command(name="daily", description="GEXP a player has earned in a day")
     @app_commands.describe(player="Player to query data for")
-    async def daily_command(self, interaction: discord.Interaction, player: str = None) -> None:
-        logging.debug(f"User {interaction.user.id} ran command '/gexp daily'")
+    async def process_daily_command(self, interaction: discord.Interaction, player: str = None) -> None:
+        user_id = interaction.user.id
+        logging.debug(f"User {user_id} ran command '/gexp daily'")
         await interaction.response.defer()
 
         if player is None:
-            discord_link = self.local_data.discord_link.get_link(interaction.user.id)
-            player = mcign.dash_uuid(discord_link.uuid)
-            if discord_link is None:
-                await interaction.edit_original_response(embed=embed_lib.InvalidArgumentEmbed())
+            player = await self.get_default_player(user_id)
+            if player is None:
+                await self.send_invalid_argument_response(interaction)
                 return
 
-        uuid = None
+        uuid = await self.get_uuid(player)
+        if uuid is None:
+            await self.send_invalid_mojang_user_response(interaction, player)
+            return
+
+        date_today = datetime.today().strftime("%Y-%m-%d")
+        gexp_today = await self.get_gexp_for_player(uuid, date_today)
+
+        if gexp_today is None:
+            await self.send_gexp_not_found_response(interaction, uuid)
+            return
+
+        await self.send_daily_gexp_response(interaction, uuid, gexp_today, date_today)
+
+    async def get_default_player(self, user_id: int) -> str:
+        discord_link = self.local_data.discord_link.get_link(user_id)
+        if discord_link:
+            return mcign.dash_uuid(discord_link.uuid)
+        return None
+
+    async def get_uuid(self, player: str) -> str:
         cache_player = self.local_data.uuid_cache.get_entry(player)
         if cache_player.is_alive:
-            uuid = mcign.dash_uuid(cache_player.uuid)
+            return mcign.dash_uuid(cache_player.uuid)
         else:
             mojang_player = MCIGN(player).uuid
             if mojang_player is None:
-                await interaction.edit_original_response(embed=embed_lib.InvalidMojangUserEmbed(player=player))
-                return
-            uuid = mcign.dash_uuid(mojang_player)
+                return None
+            return mcign.dash_uuid(mojang_player)
 
-        date_today = datetime.today().strftime("%Y-%m-%d")
+    async def get_gexp_for_player(self, uuid: str, date: str) -> int:
         cursor = self.local_data.gexp_db.cursor
         cmd = "SELECT date, amount FROM expHistory WHERE (uuid = ?) AND (date = ?)"
-        query = cursor.execute(cmd, (uuid, date_today))
+        query = cursor.execute(cmd, (uuid, date))
         result = query.fetchone()
-        if result is None:
-            await interaction.edit_original_response(
-                embed=embed_lib.PlayerGexpDataNotFoundEmbed(player=uuid))
-            return
-        gexp_today = result[1]
-        await interaction.edit_original_response(
-            embed=embed_lib.DailyGexpEmbed(uuid, uuid, result[1], result[0]))
+        if result:
+            return result[1]
+        return None
+
+    async def send_invalid_argument_response(self, interaction: discord.Interaction):
+        await interaction.edit_original_response(embed=embed_lib.InvalidArgumentEmbed())
+
+    async def send_invalid_mojang_user_response(self, interaction: discord.Interaction, player: str):
+        await interaction.edit_original_response(embed=embed_lib.InvalidMojangUserEmbed(player=player))
+
+    async def send_gexp_not_found_response(self, interaction: discord.Interaction, uuid: str):
+        await interaction.edit_original_response(embed=embed_lib.PlayerGexpDataNotFoundEmbed(player=uuid))
+
+    async def send_daily_gexp_response(self, interaction: discord.Interaction, uuid: str, gexp: int, date: str):
+        await interaction.edit_original_response(embed=embed_lib.DailyGexpEmbed(uuid, uuid, gexp, date))
 
 
 async def setup(bot: commands.Bot):
