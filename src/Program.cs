7dd030f830946fbc14593052a2b46133af38b4e3
illyum <90023277+itzilly@users.noreply.github.com>
2023-09-25 15:48:43 -0600

    Add debug testing for new class

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DiscordBot.HypixelApi;
+using Newtonsoft.Json;
 
-Console.WriteLine("[WARNING] - This repo is still under development!\n" +
-                  "[WARNING] - Features will NOT work as intended, nor will anything else!\n" +
-                  "[WARNING] - YOU HAVE BEEN WARNED");
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("[WARNING] - This repo is still under development!\n" +
+                          "[WARNING] - Features will NOT work as intended, nor will anything else!\n" +
+                          "[WARNING] - YOU HAVE BEEN WARNED");
+        
+        var jsonString = "{ \"success\": true}";
+        var response = JsonConvert.DeserializeObject<HypixelPlayerResponse>(jsonString);
+        Console.Out.WriteLine(String.Format("Player has '{0}' bedwars wins!", response.Stats.BedwarsStats.Wins));
+
+        var bot = new DiscordBot.DiscordBot();
+        bot.RunBotAsync().GetAwaiter().GetResult();
+        Console.Out.WriteLine("Bot is logging off");
+    }
+}
+
