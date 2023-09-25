dd3a1faa9e176004dcf2c7f7e1fe55d8e87de7a9
illyum <90023277+itzilly@users.noreply.github.com>
2023-09-25 15:00:05 -0600

    Create SettingsConf.cs

---
+++ src/SettingsConf.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+
+
+public class SettingsConf {
+    public SettingsConfLoader Loader { get; set; }
+    [JsonProperty("token")] public string Token { get; private set; }
+}
+
+public class SettingsConfLoader {
+    public string ConfigPath = "settings.conf";
+    public SettingsConf SettingsConf { get; private set; }
+
+    /// <summary>
+    /// Loads the configuration asynchronously
+    /// </summary>
+    /// <exception cref="InvalidOperationException"></exception>
+    public async Task LoadConfigAsync() {
+        var json = string.Empty;
+        using (var fileStream = File.OpenRead(ConfigPath))
+        using (var streamReader = new StreamReader(fileStream, new UTF8Encoding(false)))
+            json = await streamReader.ReadToEndAsync();
+        SettingsConf = JsonConvert.DeserializeObject<SettingsConf>(json) ??
+                       throw new InvalidOperationException("Error loading config (Async)");
+        SettingsConf.Loader = this;
+    }
+
+    /// <summary>
+    /// Loads the configuration synchronously
+    /// </summary>
+    /// <exception cref="InvalidOperationException"></exception>
+    public void LoadConfigSync() {
+        var json = string.Empty;
+        using (var fileStream = File.OpenRead(ConfigPath))
+        using (var streamReader = new StreamReader(fileStream, new UTF8Encoding(false)))
+            json = streamReader.ReadToEnd();
+        SettingsConf = JsonConvert.DeserializeObject<SettingsConf>(json) ??
+                       throw new InvalidOperationException("Error loading config (Sync)");
+        SettingsConf.Loader = this;
+    }
+}
