dd49d8903dc579197187f03f0c411f304df10c0b
illyum <90023277+itzilly@users.noreply.github.com>
2023-09-25 15:28:32 -0600

    Add URI Builder class

---
+++ src/HypixelApi/HypixelApiEndpointBuilder.cs
@@ -0,0 +1,22 @@
+﻿public class HypixelApiEndpointBuilder {
+    private String _prebuiltString = String.Empty;
+    private String _defaultUri = "https://api.hypixel.net/";
+
+    public void Default() {
+        _prebuiltString = _defaultUri;
+    }
+
+    public void SetPlayer(Guid UUID) {
+        _prebuiltString = _prebuiltString + "/player?uuid" + UUID;
+    }
+
+
+    public String Build() {
+        if (_prebuiltString == _defaultUri) {
+            string message = "NullReference to endpoint location (did you forget to select the endpoint?)"
+            throw new UriFormatException(message);
+        }
+
+        return _prebuiltString;
+    }
+}
