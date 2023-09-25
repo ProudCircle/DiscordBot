32870159f95ea506427ae3c60b4a289ebc78bab6
illyum <90023277+itzilly@users.noreply.github.com>
2023-09-25 02:18:27 -0600

    Remove 'util' folder

--- app/util/uuider.py
+++
@@ -1,9 +0,0 @@
-def add_hyphens_to_uuid(uuid_string):
-    formatted_uuid = '{}-{}-{}-{}-{}'.format(
-        uuid_string[:8],
-        uuid_string[8:12],
-        uuid_string[12:16],
-        uuid_string[16:20],
-        uuid_string[20:]
-    )
-    return formatted_uuid
