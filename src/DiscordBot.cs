540a07bf32c10f866d0129ff4da4e91cfbd4f271
illyum <90023277+itzilly@users.noreply.github.com>
2023-09-25 14:59:46 -0600

    Add DiscordBot class

---
+++ src/DiscordBot.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.SlashCommands;
+
+namespace DiscordBot; 
+
+/// <summary>
+/// The main discord bot
+/// </summary>
+public class DiscordBot {
+    public DiscordClient DiscordClient { get; private set; }
+    public CommandsNextExtension CommandsExtension { get; private set; }
+    public SlashCommandsExtension SlashCommandsExtension { get; private set; }
+    public SettingsConf Conf { get; private set; }
+    public string Version { get; private set; }
+    
+    /// <summary>
+    /// Starts the discord bot
+    /// </summary>
+    public async Task RunBotAsync() {
+        var settingsConfLoader = new SettingsConfLoader();
+        settingsConfLoader.LoadConfigSync();
+        Conf = settingsConfLoader.SettingsConf;
+    }
+}
