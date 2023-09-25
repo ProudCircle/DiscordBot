c00fea14633b66d7f546da24a0dcf195bc6669b4
illyum <90023277+itzilly@users.noreply.github.com>
2023-09-25 03:30:54 -0600

    Add Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("[WARNING] - This repo is still under development!\n" +
+                  "[WARNING] - Features will NOT work as intended, nor will anything else!\n" +
+                  "[WARNING] - YOU HAVE BEEN WARNED");
